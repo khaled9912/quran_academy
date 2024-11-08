478382defc36111d76fb2ed429d5e2ea1c3afacd
Khaled <khaled.mourraad@gmail.com>
2024-11-09 01:42:59 +0200

    feat: add courses images and fix logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,9 @@ import Link from 'next/link';
 import React from 'react';
 import Menu from './Menu';
 import Image from 'next/image';
-import SearchBar from './SearchBar';
-import NavIcons from './NavIcons';
+
+import LogoSVG from '/public/logo.svg';
+
 
 const Navbar = () => {
   return (
@@ -26,16 +27,14 @@ const Navbar = () => {
           {/* LINKS */}
           <div className="hidden xl:flex gap-4">
             <Link href="/">HomePage</Link>
-            <Link href="/">Shope</Link>
-            <Link href="/">Deals</Link>
-            <Link href="/">About</Link>
-            <Link href="/">Contacts</Link>
+            <Link href="#about">About Us</Link>
+            <Link href="#courses">Courses</Link>
+            <Link href="#contacts">Contacts</Link>
           </div>
         </div>
         {/* RIGHT */}
         <div className="w-2/3 xl:w-1/2 flex items-center justify-between gap-8">
-          <SearchBar />
-          <NavIcons />
+
         </div>
       </div>
     </div>
