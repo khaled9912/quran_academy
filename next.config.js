17ede4677f25ff2796f04a4215f394101b2399b4
Khaled <khaled.mourraad@gmail.com>
2024-11-09 16:33:13 +0200

    fix: add nex.config.js file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    EMAIL_USER: process.env.EMAIL_USER,
+    EMAIL_APP_PASSWORD: process.env.EMAIL_APP_PASSWORD,
+    RECEIVER_EMAIL: process.env.RECEIVER_EMAIL,
+  },
   images: {
     remotePatterns: [
       {
